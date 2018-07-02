55c7115ee258faebb77ba04eb85fa3ea39bdee74
akanksha1808 <40741360+akanksha1808@users.noreply.github.com>
2018-07-02 14:34:08 +0530

    Update crudBackendApplication.java

--- crudBackendApplication.java
+++ crudBackendApplication.java
@@ -1,12 +1,12 @@
-package com.infogain;
+package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.infogain.entities.User;
-import com.infogain.repository.UserRepository;
+import com.example.entities.User;
+import com.example.repository.UserRepository;
 
 @SpringBootApplication
 public class crudBackendApplication implements CommandLineRunner {
